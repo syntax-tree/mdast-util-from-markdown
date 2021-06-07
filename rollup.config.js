22cb6be6e01e7398ceddac022187d0652fd38384
v1rtl <pilll.pl22@gmail.com>
2021-06-07 18:19:03 +0300

    chore(esm): replace babel cli with rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import filesize from 'rollup-plugin-filesize'
+import {babel} from '@rollup/plugin-babel'
 
 const config = {
   input: 'lib/index.js',
@@ -6,7 +7,19 @@ const config = {
     format: 'esm',
     dir: 'dist'
   },
-  plugins: [filesize()]
+  plugins: [babel({babelHelpers: 'bundled'}), filesize()],
+  external: [
+    'micromark-util-symbol/constants',
+    'micromark-util-symbol/values',
+    'micromark-util-symbol/types',
+    'mdast-util-to-string',
+    'micromark-util-normalize-identifier',
+    'micromark/lib/parse',
+    'micromark/lib/preprocess',
+    'micromark/lib/postprocess',
+    'parse-entities/decode-entity',
+    'unist-util-stringify-position'
+  ]
 }
 
 export default config
